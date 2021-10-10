4e97a80ac7f1b9c3257722cf387e1b49fee8e352
AksanaZhdanovich <90707013+AksanaZhdanovich@users.noreply.github.com>
2021-10-10 13:31:56 +0300

    Update lab5.py
    ver_2

--- lab5.py
+++ lab5.py
@@ -1,66 +1,78 @@
 from datetime import datetime
 
 
-class NewNews:
-    datetime_news = datetime.now().strftime('%d/%m/%Y %H.%M')
+class NewInformation:
+    file_to_write = "newsfeed.txt"
+    length_of_description = 30
 
     def __init__(self, note_text):
         self.note = note_text
-        self.end_record = '-' * 30
+        self.end_record = '-' * self.length_of_description
         self.text_in_file = ''
-        self.type_of_news = ''
+        self.type_of_info = ''
 
-    def add_new_note_in_the_file(self):
-        with open(file_to_write, "a") as text_file:
+    def add_new_info_in_the_file(self):
+        with open(self.file_to_write, "a") as text_file:
             text_file.write(self.text_in_file + '\n\n')
         print('The information was added\n')
 
-    def news_description(self):
-        return self.type_of_news + '-' * (30 - len(self.type_of_news)) + '\n' + self.note + '\n'
+    def info_description(self):
+        return self.type_of_info + '-' * (self.length_of_description - len(self.type_of_info)) + '\n' + self.note + '\n'
 
 
-class AddNews(NewNews):
+class AddNews(NewInformation):
 
     def __init__(self, note_text, city):
-        NewNews.__init__(self, note_text=note_text)
-        self.type_of_news = 'News '
-        description = NewNews.news_description(self)
+        super().__init__(note_text=note_text)
+        self.datetime_news = datetime.now().strftime('%d/%m/%Y %H.%M')
+        self.type_of_info = 'News '
+        self.description = NewInformation.info_description(self)
         self.city = city
-        self.text_in_file = description + self.city + ',' + self.datetime_news + '\n' + self.end_record + '\n'
-        self.add_new_note_in_the_file()
+        self.text_in_file = self.description + self.city + ',' + self.datetime_news + '\n' + self.end_record + '\n'
+        self.add_new_info_in_the_file()
 
 
-class AddAd(NewNews):
+class AddAd(NewInformation):
 
     def __init__(self, note_text, date):
-        NewNews.__init__(self, note_text=note_text)
+        super().__init__(note_text=note_text)
         self.date = date
         self.expire_date = str((datetime.strptime(self.date, '%d/%m/%Y') - datetime.now()).days + 1)
-        self.type_of_news = 'Private Ad '
-        description = NewNews.news_description(self)
-        self.text_in_file = description + 'Actual until: ' + self.date + ', ' + self.expire_date + ' days left.' + '\n' + self.end_record + '\n'
-        self.add_new_note_in_the_file()
+        self.type_of_info = 'Private Ad '
+        self.description = NewInformation.info_description(self)
+        self.text_in_file = self.description + 'Actual until: ' + self.date + ', ' + self.expire_date + ' days left.' + '\n' + self.end_record + '\n'
+        self.add_new_info_in_the_file()
 
 
-class AddWeatherForecast(NewNews):
+class AddWeatherForecast(NewInformation):
     def __init__(self, note_text, city, date, degrees):
-        NewNews.__init__(self, note_text=note_text)
+        super().__init__(note_text=note_text)
         self.date = date
         self.degrees = degrees
         self.city = city
-        self.type_of_news = 'Weather Forecast'
-        description = NewNews.news_description(self)
-        self.text_in_file = description + 'Temperature: ' + self.degrees + ' in ' + self.city + ' Date:' + self.date + '\n' + self.end_record + '\n'
-        self.add_new_note_in_the_file()
+        self.type_of_info = 'Weather Forecast'
+        self.description = NewInformation.info_description(self)
+        self.text_in_file = self.description + 'Temperature: ' + self.degrees + ' in ' + self.city + ' Date:' + self.date + '\n' + self.end_record + '\n'
+        self.add_new_info_in_the_file()
+
+
+def validation_date(type_of_info):
+    while True:
+        input_date = input(f'Please write the date for {type_of_info} in format DD/MM/YYYY')
+        try:
+            datetime.strptime(input_date, '%d/%m/%Y')
+            return input_date
+        except ValueError:
+            print('Invalid date! Please try again!')
+            continue
 
 
 def main_menu():
-    type_of_feed = str()
+    type_of_feed = ''
     print(' Hi, welcome to our news feed.')
     while type_of_feed != '0':
-        print('Choose number to add info:\n', '1 - add news\n', '2 - add private ad\n', '3 - add weather forecast\n',
-              '0 - quit\n', 'Your choice:')
-        type_of_feed = input()
+        type_of_feed = input('Choose number to add info:\n' + '1 - add news\n' + '2 - add private ad\n' +
+                             '3 - add weather forecast\n' + '0 - quit\n' + 'Your choice:')
         if type_of_feed not in ['0', '1', '2', '3']:
             print(f'Your choice is "{type_of_feed}" and it is not an expected value, please try again')
         elif type_of_feed == '1':
@@ -70,29 +82,19 @@ def main_menu():
             AddNews(news_text, news_city)
         elif type_of_feed == '2':
             print('Your choice is 2 - add ad:')
-            news_text = input('Please type the text of your private ad:\n> ').capitalize()
-            news_date = input('Please type the date of your ad:(right format is DD/MM/YYYY):')
-            try:
-                valid_date = datetime.strptime(news_date, '%d/%m/%Y')
-            except ValueError:
-                print('Invalid date! Please try again!')
-                continue
-            AddAd(news_text, news_date)
+            ad_text = input('Please type the text of your private ad:\n> ').capitalize()
+            ad_date = validation_date('Ad')
+            print('ad_date', ad_date)
+            AddAd(ad_text, ad_date)
         elif type_of_feed == '3':
             print('Your choice is 3 - add weather forecast:')
-            news_text = input('Please type the forecast:').capitalize()
-            news_date = input('Please type the date of your forecast:(right format is DD/MM/YYYY):')
-            try:
-                valid_date = datetime.strptime(news_date, '%d/%m/%Y')
-            except ValueError:
-                print('Invalid date! Please try again!')
-                continue
-            news_city = input('Please type the city:').capitalize()
-            news_temperature = input('Please type temperature:')
-            AddWeatherForecast(news_text, news_city, news_date, news_temperature)
-        if type_of_feed == '0':
-            print(f'All the information is in the file {file_to_write}')
+            forecast_text = input('Please type the forecast:').capitalize()
+            forecast_date = validation_date('Weather forecast')
+            forecast_city = input('Please type the city:').capitalize()
+            forecast_temperature = input('Please type temperature:')
+            AddWeatherForecast(forecast_text, forecast_city, forecast_date, forecast_temperature)
+    else:
+        print(f'All the information is in the file {NewInformation.file_to_write}')
 
 
-file_to_write = "newsfeed.txt"
 main_menu()
